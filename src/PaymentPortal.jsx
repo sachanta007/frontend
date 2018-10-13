646302b440d8ab21affbb8cda09b86a25024afbd
Kriti <kritishree06@gmail.com>
2018-10-13 17:07:14 -0400

    SICER2-30: Payment portal for the students
    SICER2-30: Payment portal for the students

---
+++ src/PaymentPortal.jsx
@@ -0,0 +1,200 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import { Redirect } from 'react-router';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { render } from "react-dom";
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel'
+import Radio from '@material-ui/core/Radio';
+import FormHelperText from '@material-ui/core/FormHelperText';
+
+
+
+const styles = {
+  card: {
+    marginTop: 80,
+    width: 400,
+    margin: 'auto'
+  },
+  card1: {
+    marginTop: 80,
+    width: 400,
+    margin: 'auto'
+  },
+  card2: {
+    marginTop: 80,
+    width: 400,
+    margin: 'auto'
+  },
+  deleteIcon:{
+    marginTop: 20,
+    marginLeft: 350
+  },
+  marginAuto:{
+    margin:'auto'
+  }
+
+
+};
+const axios = require('axios');
+
+
+  class PaymentPortal extends React.Component {
+  constructor()
+  {
+    super();
+    this.state ={
+      firstCourse:'',
+      SecondCourse:'',
+      ThirdCourse:'',
+      TotalAmount:'',
+      isPaymentPortalCardHidden:false,
+      isPaymentModeCardHidden:true,
+      isPaymentSuccessfulCardHidden:true
+
+      }
+    }
+
+    prompt(e) {
+    alert(e);
+    console.log('The link was clicked.');
+    }
+
+    handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  PaymentMode(event) {
+    this.setState({isPaymentPortalCardHidden: true});
+    this.setState({isPaymentModeCardHidden: false});
+    this.setState({isPaymentSuccessfulCardHidden: true});
+  }
+
+  
+
+    render() {
+    const { classes } = this.props;
+    let currentCard;
+
+    if(!this.state.isPaymentPortalCardHidden){
+
+    currentCard = <div>
+    <Card className={classes.card}>
+               <DeleteIcon className={classes.icon, classes.deleteIcon} />
+              <CardContent>
+                <Typography gutterBottom variant="headline" component="h2">
+                Course 1
+                </Typography>
+                <Typography component="p">
+                  Mon, Tues 1:45
+                </Typography>
+              </CardContent>
+          </Card>
+
+          <Card className={classes.card1}>
+          <Button onClick={this.prompt.bind(this,'hello')}>
+          <DeleteIcon className={classes.icon, classes.deleteIcon} />
+          </Button>
+                    <CardContent>
+                      <Typography gutterBottom variant="headline" component="h2">
+                      Course 2
+                      </Typography>
+                      <Typography component="p">
+                        Mon, Wed 4:00
+                      </Typography>
+                    </CardContent>
+                </Card>
+
+
+                <Card className={classes.card2}>
+                        <DeleteIcon className={classes.icon, classes.deleteIcon} />
+                          <CardContent>
+                            <Typography gutterBottom variant="headline" component="h2">
+                            Course 3
+                            </Typography>
+                            <Typography component="p">
+                            Thurs, Fri 1:00
+                            </Typography>
+                          </CardContent>
+                      </Card>
+
+                      <div>
+                          <CardActions>
+                      <Button variant="contained" onClick = {this.PaymentMode.bind(this)} className = {classes.marginAuto}  color="primary">Checkout</Button>
+                         </CardActions>
+                      </div>
+          </div>
+}
+if(!this.state.isPaymentModeCardHidden)
+{
+
+currentCard=
+
+      <div className={classes.root}>
+
+
+        <FormControl component="fieldset" className={classes.formControl}>
+                  <FormLabel component="legend">Payment Mode</FormLabel>
+                  <RadioGroup
+                    aria-label="Payment Mode"
+                    name="Payment Mode"
+                    className={classes.group}
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                  >
+                    <FormControlLabel
+                      value="Credit Card"
+                      control={<Radio color="primary" />}
+                      label="Credit Card"
+                    labelPlacement="middle"
+                    />
+                    <FormControlLabel
+                      value="eCheck"
+                      control={<Radio color="primary" />}
+                      label="eCheck"
+                      labelPlacement="middle"
+                    />
+                    <FormControlLabel
+                      value="Coupon"
+                      control={<Radio color="primary" />}
+                      label="Coupon"
+                      labelPlacement="middle"
+                    />
+                  </RadioGroup>
+                  <Button variant="contained" onClick = {this.PaymentMode.bind(this)} className = {classes.marginAuto}  color="primary">Pay</Button>
+
+                </FormControl>
+
+    </div>
+
+}
+      return (
+           <div>
+             {currentCard}
+           </div>
+         );
+
+    }
+  }
+
+
+
+
+    PaymentPortal.propTypes = {
+      classes: PropTypes.object.isRequired,
+    };
+
+
+    export default withStyles(styles)(PaymentPortal);
